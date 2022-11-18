f91ec3523bd97724dc639f11db695b00899f0b5c
Ion Oleinic <ion.oleinic@student.usv.ro>
2022-11-18 14:46:58 +0200

    fara schimbari

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
-const express=require("express");
-const bodyParser =require( "body-parser");
-const  Sequelize =require("sequelize");
-const cors =require("cors");
-const nodemailer =require ("nodemailer")
-const dotenv =require ("dotenv");
+const express = require('express')
+const bodyParser = require('body-parser')
+const Sequelize = require('sequelize')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+const dotenv = require('dotenv')
 
 //db conn
-require("./database/connection");
+require('./database/connection')
 
-require("./bootstrap")();
+require('./bootstrap')()
 
-dotenv.config();
-const router = express.Router();
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+dotenv.config()
+const router = express.Router()
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(5000, () => console.log('Server Running'))
 const contactEmail = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com", // hostname
-    secureConnection: false, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
-    },
-    tls: {
-        ciphers:'SSLv3'
-    }
-});
+  host: 'smtp-mail.outlook.com', // hostname
+  secureConnection: false, // TLS requires secureConnection to be false
+  port: 587, // port for secure SMTP
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+  tls: {
+    ciphers: 'SSLv3',
+  },
+})
 
 contactEmail.verify((error) => {
   if (error) {
-    console.log(error);
+    console.log(error)
   } else {
-    console.log("Ready to Send");
+    console.log('Ready to Send')
   }
-});
+})
 
-router.post("/contact", (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const message = req.body.message; 
+router.post('/contact', (req, res) => {
+  const name = req.body.name
+  const email = req.body.email
+  const message = req.body.message
+
   const mail = {
     from: name,
-    to: "csmsuceava@outlook.com",
+    to: 'csmsuceava@outlook.com',
     subject: `Mesaj nou de la ${name}`,
     html: `<div style="font - family: system-ui; max-width:500px; margin-left">
     <h3>Ati primit un nou mesaj:</h3>
@@ -52,12 +53,12 @@ router.post("/contact", (req, res) => {
     <p><b>Email</b>: ${email}</p>
     <p><b>Mesaj</b>: ${message}</p>
     </div>`,
-  };
+  }
   contactEmail.sendMail(mail, (error) => {
     if (error) {
-      res.json({ status: "ERROR" });
+      res.json({ status: 'ERROR' })
     } else {
-      res.json({ status: "Mesaj trimis cu succes" });
+      res.json({ status: 'Mesaj trimis cu succes' })
     }
-  });
-});
+  })
+})
