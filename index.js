42043546339ce1f2ffc9fcd3603d4f02b89b4eaa
claudiu-reut <claudiu.reut@student.usv.ro>
2022-11-18 22:27:40 +0200

    added register and login endpoints

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const  Sequelize =require("sequelize");
 const cors =require("cors");
 const nodemailer =require ("nodemailer")
 const dotenv =require ("dotenv");
+const jwt=require("jsonwebtoken");
 
 //db conn
 require("./database/connection");
 
 require("./bootstrap")();
+const User=require("./models/user");
+const Sponsor=require("./models/sponsors");
+
+const errHandler= (err)=>{
+    console.error("Error: ",err);
+}
+
 
 dotenv.config();
 const router = express.Router();
@@ -60,4 +68,45 @@ router.post("/contact", (req, res) => {
       res.json({ status: "Mesaj trimis cu succes" });
     }
   });
+});
+
+
+//login endpoint
+app.post("/api/login", async (req, res) => {
+  console.log(req.body.email);
+  console.log(req.body.password);
+  const user = await User.findOne({where:{
+    email: req.body.email,
+    password: req.body.password,
+  }}).catch(errHandler);
+  if (user) {
+    const token = jwt.sign(
+      {
+        email: user.email,
+        role: user.role,
+      },
+      "secret123"
+    );
+    return res.json({status: "ok", user: token});
+  } else {
+    return res.json({status: "error", user: false});
+  }
+});
+
+//register endpoint
+app.post("/api/register", async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      role: req.body.role,
+    });
+
+    res.json({status: "ok"});
+  } catch (err) {
+    res.json({status: "error", err: err});
+  }
 });
